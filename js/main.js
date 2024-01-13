7dbf17c0c10b3468ff28b20822c75991a14b7d5a
viktoria kostrygina <vikakostrygina22@gmail.com>
2024-01-13 18:29:37 +0300

    add new js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+};
+
+/* swiper */
+
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: false,
@@ -12,3 +28,26 @@ const swiper = new Swiper('.swiper', {
     },
   
   });
+
+  /* menu */
+
+  const header__burger = document.getElementById('header-burger');
+  const menu__main = document.getElementById('menu-main');
+  const menu__exit = document.getElementById('menu-exit')
+
+  header__burger.addEventListener('click', () => {
+     menu__main.classList.toggle('active')
+  });
+
+  menu__exit.addEventListener('click', () => {
+    menu__main.classList.remove('active')
+ });
+
+ /* search */
+
+ const btn__search = document.getElementById('btn-search');
+ const header__search = document.querySelector('.header__search');
+
+ btn__search.addEventListener('click', () => {
+  header__search.classList.toggle('header--active')
+ })
